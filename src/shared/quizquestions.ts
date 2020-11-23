7741f748603e4ab1adc28ee5c861e8ac1e8f8964
Sam <sammystinky@gmail.com>
2020-11-23 14:18:52 -0500

    All Questions

--- src/shared/quizquestions.ts
+++ src/shared/quizquestions.ts
@@ -2,7 +2,183 @@ import { Question, Saboteur } from './models/quiz.model';
 
 export const QUESTIONS: Question[] = [
   {
-    text: 'I have high discomfort with conflict and avoid dealing with it',
+    text: 'I have high discomfort with conflict and avoid dealing with it.',
     theme: Saboteur.A
+  }
+  {
+    text: 'I can be too much of a stickler or perfectionist.',
+    theme: Saboteur.S
+  }
+  {
+    text: 'I need to be constantly busy.',
+    theme: Saboteur.R
+  }
+  {
+    text: 'I am usually more anxious and worried than others around me.',
+    theme: Saboteur.HV
+  }
+  {
+    text: 'I have a strong need to take charge and be in control.',
+    theme: Saboteur.C
+  }
+  {
+    text: 'I take particular pride in being rational and analytical.',
+    theme: Saboteur.HR
+  }
+  {
+    text: 'I measure my self-worth mostly through my achievements',
+    theme: Saboteur.HA
+  }
+  {
+    text: 'I am a "pleaser.',
+    theme: Saboteur.P
+  }
+  {
+    text: 'I procrastinate a lot.',
+    theme: Saboteur.A
+  }
+  {
+    text: 'I often find I have to work harder to make up for other people\'\s laziness.',
+    theme: Saboteur.S
+  }
+  {
+    text: 'I need to juggle a lot of balls to prevent boredom.',
+    theme: Saboteur.R
+  }
+  {
+    text: 'I am hyper-vigilant and always on the lookout for danger.',
+    theme: Saboteur.HV
+  }
+  {
+    text: 'Others have described me as too controlling.',
+    theme: Saboteur.C
+  }
+  {
+    text: 'I have a tendency to feel sorry for myself.',
+    theme: Saboteur.V
+  }
+  {
+    text: 'I can come across as too analytical and intellectually arrogant.',
+    theme: Saboteur.HR
+  }
+  {
+    text: 'Being liked is very important to me.',
+    theme: Saboteur.P
+  }
+  {
+    text: 'I avoid dealing with conflicts to a point where they fester and become real problems.',
+    theme: Saboteur.A
+  }
+    {
+    text: 'Status and my public image are important to me.',
+    theme: Saboteur.HA
+  }
+  {
+    text: 'I am often anxious about potential dangers around me.',
+    theme: Saboteur.HV
+  }
+  {
+    text: 'I like things to be very orderly and organized.',
+    theme: Saboteur.S
+  }
+  {
+    text: 'I feel restless and rarely at peace with what I am doing in the moment.',
+    theme: Saboteur.R
+  }
+  {
+    text: 'I can be confrontational and forceful when I need to get things done.',
+    theme: Saboteur.C
+  }
+  {
+    text: 'When I am criticized or unfairly treated, I tend to withdraw, pout, or sulk.',
+    theme: Saboteur.V
+  }
+  {
+    text: 'When bad things happen, I tend to stay upset for some time.',
+    theme: Saboteur.V
+  }
+  {
+    text: 'I tend to intimidate others with the intensity of my analytical mind.',
+    theme: Saboteur.HR
+  }
+  {
+    text: 'Compared to others, I do more pleasing, rescuing, or flattering people.',
+    theme: Saboteur.P
+  }
+  {
+    text: 'I believe that life is about achieving and producing results.',
+    theme: Saboteur.HA
+  }
+  {
+    text: 'Others tell me I am too much of a perfectionist.',
+    theme: Saboteur.S
+  }
+  {
+    text: 'I procrastinate on dealing with important but unpleasant tasks.',
+    theme: Saboteur.A
+  }
+  {
+    text: 'Others tell me I worry too much.',
+    theme: Saboteur.HV
+  }
+  {
+    text: 'I get impatient with others easily and step in to push things forward.',
+    theme: Saboteur.C
+  }
+  {
+    text: 'I easily get impatient and bored and want something different.',
+    theme: Saboteur.R
+  }
+  {
+    text: 'I can be perceived as cold and too rational.',
+    theme: Saboteur.HR
+  }
+  {
+    text: 'Sometimes I feel like a victim or martyr.',
+    theme: Saboteur.V
+  }
+  {
+    text: 'I feel worthy mostly when I am succesful and achieving.',
+    theme: Saboteur.HA
+  }
+  {
+    text: 'I am so into helping others that I sometimes lose sight of my own needs and feel resentful.',
+    theme: Saboteur.P
+  }
+  {
+    text: 'I need to be the best at what I do.',
+    theme: Saboteur.HA
+  }
+  {
+    text: 'I often downplay the importance of some real problems.',
+    theme: Saboteur.A
+  }
+  {
+    text: 'I hate mistakes.',
+    theme: Saboteur.S
+  }
+  {
+    text: 'I am easily distracted and often scattered.',
+    theme: Saboteur.R
+  }
+  {
+    text: 'I am constantly suspicious about what other people are up to.',
+    theme: Saboteur.HV
+  }
+  {
+    text: 'I\'\ve been told I can be intimidating.',
+    theme: Saboteur.C
+  }
+  {
+    text: 'I am a private person and don\'\t often let others know my deeper feelings.',
+    theme: Saboteur.HR
+  }
+  {
+    text: 'No one understands how tough my struggles have been.',
+    theme: Saboteur.V
+  }
+  {
+    text: 'I\'\m the kind of person that puts the needs of others ahead of my own.',
+    theme: Saboteur.P
   }
 ];
